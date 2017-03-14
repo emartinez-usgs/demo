8129051a360ab977048d946f548058a7af410601
emartinez-usgs <emartinez@usgs.gov>
2017-03-13 18:24:57 -0600

    Added a test suite.

---
+++ index.spec.js
@@ -0,0 +1,14 @@
+/* global describe, it */
+'use strict';
+
+
+var expect = require('chai').expect;
+
+
+describe('Test Suite', function () {
+  describe('Unit Test', function () {
+    it('passes the test', function () {
+      expect(true).to.be.true;
+    });
+  });
+});
